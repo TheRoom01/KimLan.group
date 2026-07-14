1e2630a931b3a7e62b2cad02245408458f982e6d
Kim Lân <voduongngoclan@gmail.com>
2026-07-14 19:48:39 +0700

    style: load shared room visual theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 
 import "./globals.css";
+import "./room-share-visual-sync.css";
 import AuthControls from "@/components/AuthControls";
 import ClientErrorOverlay from './_debug/ClientErrorOverlay';
 
